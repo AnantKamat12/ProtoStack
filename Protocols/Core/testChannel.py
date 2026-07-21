39d536dfd400660845d7bbcd839ff1f0dd11939b
Anantkamat12 <anantskamat12@gmail.com>
2026-07-21 23:49:54 +0100

    a small fix

--- Protocols/Core/testChannel.py
+++ Protocols/Core/testChannel.py
@@ -2,7 +2,7 @@ from VirtualChannel import VirtualChannel
 print(__file__)
 def run_test():
     # Instantiate channel in test script
-    chan = VirtualChannel(byte_drop_rate=0,bit_flip_rate=0.9)
+    chan = VirtualChannel(byte_drop_rate=0,bit_flip_rate=0.0)
     
     chan.send(b"This is test channel")
     print("Buffer length:", len(chan._queue))
